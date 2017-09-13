568c3aa3cf739cefe2e27de5d3a3c40c16dd03db
Jose Martinez <sotero.valdez@gmail.com>
2017-09-13 18:36:00 -0500

    set as initial project

--- routes/app.js
+++ routes/app.js
@@ -5,13 +5,4 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
-router.get('/message/:msg', function (req, res, next) {
-    res.render('node', {message: req.params.msg});
-});
-
-router.post('/message', function(req, res, next) {
-    var message = req.body.message;
-    res.redirect('/message/' + message);
-});
-
 module.exports = router;
