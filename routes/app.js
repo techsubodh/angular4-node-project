5757a3b3d798cbf81d97c28bcfd255e542185de1
Jose Martinez <sotero.valdez@gmail.com>
2017-09-14 16:30:29 -0500

    end of MongoDB section

--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    User.findOne({}, function(err, doc){
+        if(err) {
+            return res.send('Error!');
+        }
+        res.render('node', {email: doc.email});
+    });
 });
 
-router.get('/message/:msg', function (req, res, next) {
-    res.render('node', {message: req.params.msg});
-});
-
-router.post('/message', function(req, res, next) {
-    var message = req.body.message;
-    res.redirect('/message/' + message);
+router.post('/', function(req, res, next) {
+    var email = req.body.email;
+    var user = new User({
+        firstName: 'Max',
+        lastName: 'Schwarz',
+        password: 'super-secret',
+        email: email
+    });
+    user.save();
+    res.redirect('/');
 });
 
 module.exports = router;
